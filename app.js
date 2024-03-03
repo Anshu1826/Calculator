195837715c07a31d713762ac11b35d00616392f9
ANSHIKA SHARMA <94789572+Anshu1826@users.noreply.github.com>
2024-03-03 11:38:09 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,29 +1,26 @@
-
-const keys = document.querySelectorAll('.key')
+const keypad = document.querySelector('.keypad');
 const screen = document.querySelector('.screen');
+const keys = document.querySelectorAll('.key');
 for(let key of keys){
     key.addEventListener('click',(e)=>{
         const keyText = e.target.innerText;
         console.log(keyText);
-        if(keyText==='C'){
-            screen.value='';
-        }
-        else if(keyText==='^'){
-            screen.value += '**';
-        }
-        else if (keyText==='D'){
-            screen.value = screen.value.slice(0,screen.value.length-1);
-        }
-        else if(keyText === '='){
+        console.log(e.keyCode);
+        if(keyText==="AC")
+        screen.value="";
+        else if(keyText==="D")
+        screen.value=screen.value.slice(0,screen.value.length-1);
+        else if(keyText==="+/-")
+        screen.value=-1*screen.value;
+        else if(keyText==="="){
             try{
-            screen.value = eval(screen.value);
+                screen.value=eval(screen.value);
             }
             catch(err){
-                screen.value = "Invalid Operation"
+                screen.value = "Invalid Operation";
             }
         }
-        else{
-            screen.value += keyText;
-        }
+        else
+        screen.value+=keyText;
     })
 }
